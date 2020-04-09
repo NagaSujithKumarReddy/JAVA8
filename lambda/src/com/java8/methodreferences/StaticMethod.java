746db93dbce66493a29b3c3f50703ca27f56033a
NagaSujithKumarReddy <56672133+NagaSujithKumarReddy@users.noreply.github.com>
2020-04-09 15:58:20 +0530

    Add files via upload

---
+++ lambda/src/com/java8/methodreferences/StaticMethod.java
@@ -0,0 +1,17 @@
+package com.methodreferences;
+interface Sayable{  
+    void say();  
+}  
+public class StaticMethod {
+	 public static void saySomething(){  
+	        System.out.println("Hello, this is static method.");  
+	    }  
+	public static void main(String[] args) {
+		// Referring static method  
+        Sayable sayable = StaticMethod::saySomething;  
+        // Calling interface method  
+        sayable.say(); 
+
+	}
+
+}
