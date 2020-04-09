2b82632290de63ae7bcdf3217a6055398d6e5482
NagaSujithKumarReddy <56672133+NagaSujithKumarReddy@users.noreply.github.com>
2020-04-09 15:46:59 +0530

    Add files via upload

---
+++ methodreferences/ConstructorReference.java
@@ -0,0 +1,17 @@
+package com.methodreferences;
+
+interface Messageable{  
+    Message getMessage(String msg);  
+}  
+class Message{  
+    Message(String msg){  
+        System.out.print(msg);  
+    }  
+}  
+public class ConstructorReference {  
+    public static void main(String[] args) {  
+        Messageable hello = Message::new;  
+        hello.getMessage("Hello");  
+    }  
+}  
+
